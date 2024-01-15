b616277c22cb2f9e3413b9d48a05cf5a59b23ea2
Eoin Lee <88650074+eoinli@users.noreply.github.com>
2024-01-15 21:20:41 +0800

    Update #24

---
+++ JOI/[JOISC 2014] Straps.cpp
@@ -0,0 +1,52 @@
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+#define endl '\n'
+
+using namespace std;
+using PII = pair<int, int>;
+
+const int N = 2e3 + 10;
+const int M = 4e3 + 10;  // 开到 4000 是为了转移时避免 (j - 1 + hk) 数组越界
+const int INF = 0xCFCFCFCF;
+
+int n;
+PII d[N];
+int f[N][M];
+// f[i][j] 表示挂好了前 i 个物品，剩余 j 个挂钩未使用时的最大喜悦值
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    memset(f, 0xCF, sizeof f);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> d[i].first >> d[i].second;
+
+    sort(d + 1, d + n + 1, greater<PII>());
+
+    f[0][1] = 0;  // 手机上最开始本身有一个挂钩，此时作为初始状态
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 0; j <= n << 1; j++) {
+            f[i][j] = max(f[i][j], f[i - 1][j]);  // 如果当前没有能挂的钩子就直接继承此前的状态
+
+            if (j) {  // 如果有能挂的钩子
+                auto [hk, val] = d[i];
+
+                // 就更新当前状态的最大喜悦值为 (前 i - 1 个物品的最大喜悦值) 与
+                // (前 i - 1 个物品的最大喜悦值 + 当前物品 i 的喜悦值) 中的较大值
+                f[i][j - 1 + hk] = max(f[i - 1][j - 1 + hk], f[i - 1][j] + val);
+            }
+        }
+
+    int ans = INF;
+    for (int i = 0; i <= n << 1; i++)
+        ans = max(ans, f[n][i]);
+
+    cout << ans << endl;
+    fflush(stdout);
+    return 0;
+}
---
+++ 洛谷题库/[洛谷 P9743] 旅行.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+#define endl '\n'
+
+using namespace std;
+using lng = long long;
+
+const int N = 50;
+const int M = 95;
+const int MOD = 998244353;
+
+int n, m, K;
+int a[N][N], b[N][N];
+lng f[2][N][N][N][M];
+// f[x][y][l][z][p] 表示从 (1, 1) 到 (x, y) 时购买 l 张 L 公司票、z 张 Z 公司票且有 p 元钱的方案数
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m >> K;
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            cin >> a[i][j];
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            cin >> b[i][j];
+
+    f[1][1][0][0][K] = 1;
+
+    int cur = 1;
+    for (int x = 1; x <= n; x++, cur ^= 1) {
+        for (int y = 1; y <= m; y++) {
+            for (int l = 0; l < n; l++)
+                for (int z = 0; z < m; z++)
+                    for (int p = 0; p <= K; p++) {
+                        lng& st = f[cur][y][l][z][p];
+
+                        // 从当前位置的上方转移到当前位置，用一张 L 票
+                        if (x > 1) st += f[cur ^ 1][y][l + 1][z][p];
+
+                        // 从当前位置的左方转移到当前位置，用一张 Z 票
+                        if (y > 1) st += f[cur][y - 1][l][z + 1][p];
+
+                        // 在当前位置购买了一张 L 票而转移到当前状态
+                        if (l > 0 && p + a[x][y] <= K)
+                            st += f[cur][y][l - 1][z][p + a[x][y]];
+
+                        // 在当前位置购买了一张 Z 票而转移到当前状态
+                        if (z > 0 && p + b[x][y] <= K)
+                            st += f[cur][y][l][z - 1][p + b[x][y]];
+
+                        // 减去重复购买的情况
+                        if (l > 0 && z > 0 && p + a[x][y] + b[x][y] <= K)
+                            st += MOD - f[cur][y][l - 1][z - 1][p + a[x][y] + b[x][y]];
+
+                        st %= MOD;  // 卡常
+                    }
+
+            cout << f[cur][y][0][0][0] << ' ';
+        }
+
+        cout << endl;
+
+        // 滚动数组清零
+        for (int y = 1; y <= m; y++)
+            for (int l = 0; l <= n; l++)
+                for (int z = 0; z <= m; z++)
+                    for (int p = 0; p <= K; p++)
+                        f[cur ^ 1][y][l][z][p] = 0;
+    }
+
+    fflush(stdout);
+    return 0;
+}
