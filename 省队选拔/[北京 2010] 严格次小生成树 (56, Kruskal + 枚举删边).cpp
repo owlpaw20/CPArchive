d883fc702735e2ae4e8d96c37bb0982e53f07f99
Eoin Lee <88650074+eoinli@users.noreply.github.com>
2024-02-22 10:08:30 +0800

    Update #48

---
+++ 省队选拔/[北京 2010] 严格次小生成树 (56, Kruskal + 枚举删边).cpp
@@ -0,0 +1,90 @@
+#include <tuple>
+#include <vector>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+
+#define endl '\n'
+
+using namespace std;
+using lng = long long;
+using TUP = tuple<int, int, int>;
+
+const int N = 1e5 + 5;
+
+int n, m;
+vector<TUP> edges;
+
+unordered_set<int> used;
+
+namespace UnionFind {
+    int fa[N];
+    void init(int n) { iota(fa, fa + n + 1, 0); }
+    int find(int x) { return x == fa[x] ? x : (fa[x] = find(fa[x])); }
+    bool merge(int x, int y) {
+        x = find(x), y = find(y);
+        if (x == y) return false;
+        return fa[x] = y, true;
+    }
+}  // namespace UnionFind
+
+lng Kruskal1() {
+    lng ans = 0;
+    int cnt = 0;
+    UnionFind::init(n);
+
+    for (size_t i = 0; i < edges.size(); i++) {
+        if (cnt == n - 1) continue;
+        auto [w, u, v] = edges[i];
+        if (UnionFind::merge(u, v))
+            used.insert(i), ans += w, cnt += 1;
+    }
+
+    if (cnt != n - 1) return (1ULL << 63) - 1;
+    return ans;
+}
+
+lng Kruskal2(int excluded) {
+    lng ans = 0;
+    int cnt = 0;
+    UnionFind::init(n);
+
+    for (size_t i = 0; i < edges.size(); i++) {
+        if (excluded == (int)i) continue;
+        if (cnt == n - 1) continue;
+        auto [w, u, v] = edges[i];
+        if (UnionFind::merge(u, v))
+            ans += w, cnt += 1;
+    }
+
+    if (cnt != n - 1) return (1ULL << 63) - 1;
+    return ans;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    while (m--) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        if (x == y) continue;
+        edges.emplace_back(z, x, y);
+    }
+
+    sort(edges.begin(), edges.end());
+
+    int MST = Kruskal1();
+
+    lng ans = (1ULL << 63) - 1;
+    for (int edge : used) {
+        lng x = Kruskal2(edge);
+        if (x == MST) continue;
+        ans = min(ans, x);
+    }
+
+    cout << ans << endl;
+    return fflush(stdout), 0;
+}
