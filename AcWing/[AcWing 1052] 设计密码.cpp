3e3594812a9754e89d338420a39c798f3aa9ef95
Eoin Lee <88650074+eoinli@users.noreply.github.com>
2024-04-08 21:11:35 +0800

    Update #65

---
+++ AcWing/[AcWing 1052] 设计密码.cpp
@@ -0,0 +1,61 @@
+// 参考题解：https://www.acwing.com/solution/content/55449/
+
+#include <string>
+#include <cstdint>
+#include <iostream>
+
+#define endl '\n'
+
+using str = std::string;
+
+const int MAX_N = 55;
+const int MOD = 1e9 + 7;
+
+int N;
+str T;
+
+struct KMP {
+    str pattern;
+    int pi[MAX_N];
+
+    void build(str pat) {
+        pattern = pat;
+        int len = pat.length();
+        for (int i = 1, j = 0; i < len; ++i) {
+            while (j > 0 && pat[i] != pat[j]) j = pi[j - 1];
+            if (pat[i] == pat[j]) ++j;
+            pi[i] = j;
+        }
+    }
+} kmp;
+
+// f[i][j] = 长度为 i 的密码，且后缀与模式串匹配的最大长度为 j 的方案数
+int f[MAX_N][MAX_N];
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> N >> T;
+    int M = T.length();
+
+    kmp.build(T);
+
+    f[0][0] = 1;
+
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < M; ++j)
+            for (int c = 'a', p = j; c <= 'z'; ++c) {
+                while (p > 0 && c != T[p]) p = kmp.pi[p - 1];
+                if (c == T[p]) ++p;
+                f[i + 1][p] = (f[i + 1][p] + f[i][j]) % MOD;
+                p = j;
+            }
+
+    int ans = 0;
+    for (int i = 0; i < M; ++i)
+        ans = (ans + f[N][i]) % MOD;
+
+    std::cout << ans << endl;
+    return fflush(stdout), 0;
+}
---
+++ USACO Contests/[USACO 2008 FEB G] Making the Grade.cpp
@@ -0,0 +1,59 @@
+// 重要引理的证明请见《算法竞赛进阶指南》新版第 294 页
+// 结论：一定存在一种最优情况，使得 B 序列中的每一个值都在 A 序列中出现过。
+
+#include <limits>
+#include <iostream>
+#include <algorithm>
+
+#define endl '\n'
+
+const int MAX_N = 2e3 + 5;
+
+int N;
+int a[MAX_N], b[MAX_N];
+int f[MAX_N][MAX_N]; // f[i][j] 表示当第 i 条道路的高度为 b[j] 时，修理完前 i 条路所需的花费
+int g[MAX_N][MAX_N]; // g[i][j] 表示 f[i][k] where k in [1, j) 的最小值
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> N;
+
+    for (int i = 1; i <= N; ++i)
+        std::cin >> a[i], b[i] = a[i];
+
+    // 如果最优解是单调不降的
+    std::sort(b + 1, b + N + 1);
+
+    for (int i = 1; i <= N; ++i) {
+        g[i][0] = std::numeric_limits<int>::max();
+        for (int j = 1; j <= N; ++j) {
+            f[i][j] = g[i - 1][j] + abs(a[i] - b[j]);
+            g[i][j] = std::min(g[i][j - 1], f[i][j]);
+        }
+    }
+
+    int ans_1 = g[N][N];
+
+    // 如果最优解是单调不增的
+    for (int i = 0; i <= N; ++i)
+        for (int j = 0; j <= N; ++j)
+            f[i][j] = g[i][j] = 0;
+
+    std::reverse(b + 1, b + N + 1);
+
+    for (int i = 1; i <= N; ++i) {
+        g[i][0] = std::numeric_limits<int>::max();
+        for (int j = 1; j <= N; ++j) {
+            f[i][j] = g[i - 1][j] + abs(a[i] - b[j]);
+            g[i][j] = std::min(g[i][j - 1], f[i][j]);
+        }
+    }
+
+    int ans_2 = g[N][N];
+
+    // 取单调不降和单调不增两种情况中的较优解
+    std::cout << std::min(ans_1, ans_2) << endl;
+    return fflush(stdout), 0;
+}
